086eda9e3de349bc33f8f26df37cf2261a704e46
Carlos Jenkins <carlos@jenkins.co.cr>
2016-06-30 00:37:36 -0600

    Minor style changes.

--- sphinx_huzzler/__init__.py
+++ sphinx_huzzler/__init__.py
@@ -6,44 +6,47 @@ import os
 import xml.etree.ElementTree as ET
 
 from docutils import nodes
-from sphinx.locale import admonitionlabels
 from sphinx.writers.html import HTMLTranslator as SphinxHTMLTranslator
-
-from pygments.style import Style
-from pygments.token import Keyword, Name, Comment, String, Error, \
-     Number, Operator, Generic, Whitespace, Punctuation, Other, Literal
 
 
 def setup(app):
-    """Setup conntects events to the sitemap builder"""
+    """Setup connects events to the sitemap builder"""
     app.connect('html-page-context', add_html_link)
     app.connect('build-finished', create_sitemap)
     app.sitemap_links = []
 
 
 def add_html_link(app, pagename, templatename, context, doctree):
-    """As each page is built, collect page names for the sitemap"""
+    """
+    As each page is built, collect page names for the sitemap
+    """
+
     base_url = app.config['html_theme_options'].get('base_url', '')
     if base_url:
         app.sitemap_links.append(base_url + pagename + ".html")
 
 
 def create_sitemap(app, exception):
-    """Generates the sitemap.xml from the collected HTML page links"""
-    if (not app.config['html_theme_options'].get('base_url', '') or
-           exception is not None or
-           not app.sitemap_links):
+    """
+    Generates the sitemap.xml from the collected HTML page links
+    """
+
+    if (
+        not app.config['html_theme_options'].get('base_url', '') or
+        exception is not None or
+        not app.sitemap_links
+    ):
         return
 
-    filename = app.outdir + "/sitemap.xml"
-    print("Generating sitemap.xml in %s" % filename)
+    filename = app.outdir + '/sitemap.xml'
+    print('Generating sitemap.xml in {}'.format(filename))
 
-    root = ET.Element("urlset")
-    root.set("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9")
+    root = ET.Element('urlset')
+    root.set('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9')
 
     for link in app.sitemap_links:
-        url = ET.SubElement(root, "url")
-        ET.SubElement(url, "loc").text = link
+        url = ET.SubElement(root, 'url')
+        ET.SubElement(url, 'loc').text = link
 
     ET.ElementTree(root).write(filename)
 
@@ -58,10 +61,11 @@ class HTMLTranslator(SphinxHTMLTranslator):
     """
     def visit_table(self, node, name=''):
         """
-        Override docutils default table formatter to not include a border
-        and to use Bootstrap CSS
+        Override docutils default table formatter to not include a border and
+        to use Bootstrap CSS
+
         See: http://sourceforge.net/p/docutils/code/HEAD/tree/trunk/docutils/docutils/writers/html4css1/__init__.py#l1550
-        """
+        """  # noqa
         self.context.append(self.compact_p)
         self.compact_p = True
         classes = 'table table-bordered ' + self.settings.table_style
@@ -99,9 +103,11 @@ class HTMLTranslator(SphinxHTMLTranslator):
         self.body.append(self.starttag(node, 'dd', '', CLASS='field-body'))
         self.set_class_on_child(node, 'first', 0)
         field = node.parent
-        if (self.compact_field_list or
+        if (
+            self.compact_field_list or
             isinstance(field.parent, nodes.docinfo) or
-            field.parent.index(field) == len(field.parent) - 1):
+            field.parent.index(field) == len(field.parent) - 1
+        ):
             # If we are in a compact list, the docinfo, or if this is
             # the last field of the field list, do not add vertical
             # space after last element.
@@ -115,8 +121,10 @@ class HTMLTranslator(SphinxHTMLTranslator):
         self.compact_p = None
         if 'compact' in node['classes']:
             self.compact_field_list = True
-        elif (self.settings.compact_field_lists
-              and 'open' not in node['classes']):
+        elif (
+            self.settings.compact_field_lists and
+            'open' not in node['classes']
+        ):
             self.compact_field_list = True
         if self.compact_field_list:
             for field in node:
@@ -130,9 +138,10 @@ class HTMLTranslator(SphinxHTMLTranslator):
                                    (nodes.paragraph, nodes.line_block))):
                     self.compact_field_list = False
                     break
-        self.body.append(self.starttag(node, 'dl', frame='void',
-                                       rules='none',
-                                       CLASS='docutils field-list'))
+        self.body.append(self.starttag(
+            node, 'dl', frame='void', rules='none',
+            CLASS='docutils field-list'
+        ))
 
     def depart_field_list(self, node):
         self.body.append('</dl>\n')
